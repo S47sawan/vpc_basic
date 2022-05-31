3a540d327011ce8f3f904fbb0b7b21979de52903
s47sawan <smihah270@yahoo.com>
2022-05-31 09:37:03 +0100

    commit

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_security_group" "bastion" {
 resource "aws_instance" "bastion" {
   ami                         = var.ami[var.region]
   instance_type               = var.bastion_instance_type
-  key_name                    = var.key_name
+  key_name                    = aws_key_pair.web.id
   monitoring                  = true
   vpc_security_group_ids      = [aws_security_group.bastion.id]
   subnet_id                   = aws_subnet.public[0].id
