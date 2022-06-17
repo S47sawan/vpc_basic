591f45faeb02140fec568faae6a341dd5250bf09
s47sawan <smihah270@yahoo.com>
2022-06-17 11:13:37 +0100

    commit

--- locals.tf
+++ locals.tf
@@ -1,6 +1,8 @@
-# locals {
-#   common_tags = {
-#       environment = "test"
-#       Owner = "sa"
-#   }
-# }
+locals {
+  // common tags to be assigned to all resources//
+  common_tags = {
+    Project     = "Terraform-book"
+    Owner       = "Smihah.Awan"
+    environment = "web"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,32 +1,29 @@
-resource "aws_vpc" "tfb" {
+resource "aws_vpc" "sa-web-vpc" {
   cidr_block           = var.cidr
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support   = var.enable_dns_support
-  tags = {
-    Name = var.name
-  }
+ 
+  tags =merge(locals.common_tags,{Name = "sa-vpc_basic-web"})
 }
 
-resource "aws_internet_gateway" "tfb" {
+resource "aws_internet_gateway" "sa-web-igw" {
   vpc_id = aws_vpc.tfb.id
-  tags = {
-    Name = "${var.name}-igw"
+  tags =merge(locals.common_tags,{Name = sa-web-igw})
   }
-}
+
 
 resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.tfb.main_route_table_id
+  route_table_id         = aws_vpc.sa-web-vpc.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.tfb.id
+  gateway_id             = aws_internet_gateway.sa-web-igw.id
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.tfb.id
+  vpc_id                  = aws_vpc.sa-web-vpc.id
   cidr_block              = var.public_subnet
   map_public_ip_on_launch = var.map_public_ip_on_launch
-  tags = {
-    Name = "${var.name}-public"
-  }
+  
+  tags =merge(locals.common_tags,{Name = "sa-public-subnet"}
 }
 
 
